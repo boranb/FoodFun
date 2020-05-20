f3c2df9862c8cadd42d03a1fe38b9aae3c512373
boranb <berkayboran@gmail.com>
2020-05-21 01:19:49 +0300

    Category and Products

--- FoodFun/Controllers/HomeController.cs
+++ FoodFun/Controllers/HomeController.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FoodFun.ViewModels;
 
 namespace FoodFun.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            return View();
+            var vm = new HomeIndexViewModel()
+            {
+                Products = db.Products.OrderByDescending(x=>x.CreationTime).ToList()
+            };
+            return View(vm);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            ViewBag.Message = "Your menu page.";
 
             return View();
         }
@@ -26,6 +38,12 @@ namespace FoodFun.Controllers
 
             return View();
         }
-        
+
+        public ActionResult CategoriesPartial()
+        {
+            var cats = db.Categories.OrderBy(x => x.CategoryName).ToList();
+            return PartialView("_CategoriesPartial", cats);
+        }
+
     }
 }
---
+++ FoodFun/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FoodFun.Models;
+
+namespace FoodFun.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
